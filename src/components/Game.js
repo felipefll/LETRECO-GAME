6ad16db0d57d59a79f60b8d95ff0b0ccd3b27dfa
felipefll <felipeferreiralopeslima@gmail.com>
2023-03-06 14:00:01 -0300

    word correction

--- src/components/Game.js
+++ src/components/Game.js
@@ -29,7 +29,7 @@ const Game = ({
       <p className="points">
         <span>Pontuação: {score}</span>
       </p>
-      <h1>Advinhe a palavra</h1>
+      <h1>Adivinhe a palavra</h1>
       <h3 className="tip">
         Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
@@ -46,7 +46,7 @@ const Game = ({
         )}
       </div>
       <div className="letterContainer">
-        <p>Tente advinhar uma letra da palavra:</p>
+        <p>Tente adivinhar uma letra da palavra:</p>
 
         <form onSubmit={handleSubmit}>
           <input
